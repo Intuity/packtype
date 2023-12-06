31f5cb1a6f04db02e2d70209be33e0b623a94591
Peter Birch <peter.birch@vypercore.com>
2023-12-06 10:46:03 +0000

    Expanding enumeration handling

--- packtype/enum.py
+++ packtype/enum.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import enum
+import math
 from typing import Any
 
 from .assembly import Assembly
@@ -20,18 +21,67 @@ from .assembly import Assembly
 class EnumMode(enum.Enum):
     INDEXED = enum.auto()
     ONE_HOT = enum.auto()
-    GREY = enum.auto()
+    GRAY = enum.auto()
+
+
+class EnumError(Exception):
+    pass
+
 
 class Enum(Assembly):
     _PT_ATTRIBUTES: dict[str, tuple[Any, list[Any]]] = {
         "mode": (EnumMode.INDEXED, list(EnumMode)),
+        "width": (-1, lambda x: x > 0),
     }
 
     def __init__(self) -> None:
         super().__init__()
         self._pt_mode = self._PT_ATTRIBUTES["mode"]
-        next_val = 0
+        self._pt_width = self._PT_ATTRIBUTES["width"]
+        # Indexed
+        if self._pt_mode is EnumMode.INDEXED:
+            next_val = 0
+            for _, fval in self._pt_fields:
+                if fval.value is None:
+                    fval.value = next_val
+                next_val = fval.value + 1
+        # One-hot
+        elif self._pt_mode is EnumMode.ONE_HOT:
+            next_val = 1
+            for fname, fval in self._pt_fields:
+                if fval.value is None:
+                    fval.value = next_val
+                if (math.log2(fval.value) % 1) != 0:
+                    raise EnumError(
+                        f"Enum entry {fname} has value {fval.value} that is not "
+                        f"one-hot encoded"
+                    )
+                next_val = (fval.value << 1)
+        # Gray code
+        elif self._pt_mode is EnumMode.GRAY:
+            for idx, (fname, fval) in self._pt_fields:
+                gray_val = (idx ^ (idx >> 1))
+                if fval.value is None:
+                    fval.value = gray_val
+                elif fval.value != gray_val:
+                    raise EnumError(
+                        f"Enum entry {fname} has value {fval.value} that does "
+                        f"not conform to the expected Gray code value of {gray_val}"
+                    )
+        # Determine width
+        if self._pt_width < 0:
+            self._pt_width = int(math.ceil(math.log2(max(x[1].value for x in self._pt_fields)+1)))
+        # Check for oversized or repeated values
+        used = []
+        max_val = (1 << self._pt_width) - 1
         for fname, fval in self._pt_fields:
-            if fval.value is None:
-                fval.value = next_val
-                next_val += 1
+            if fval.value > max_val:
+                raise EnumError(
+                    f"Enum entry {fname} has value {fval.value} that cannot be "
+                    f"encoded in a bit width of {self._pt_width}"
+                )
+            elif fval.value in used:
+                raise EnumError(
+                    f"Enum entry {fname} has value {fval.value} that appears "
+                    f"more than once in the enumeration"
+                )
