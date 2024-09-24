e9278ddc4d0b41ff1f2052202f342401f1e31cf2
Peter Birch <peter.birch@vypercore.com>
2024-09-24 10:20:13 +0100

    Reading back status

--- examples/registers/testbench/testcases/access.py
+++ examples/registers/testbench/testcases/access.py
@@ -12,18 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cocotb.triggers import RisingEdge, ClockCycles
+from cocotb.triggers import RisingEdge
 from forastero import MonitorEvent
 
-from registers import Control, Identity, Version
+from registers import Control, Version, Status
 
 from ..regintf import RegRequest, RegResponse
 from ..testbench import Testbench
 
 
 @Testbench.testcase()
-async def access_identity(tb, log):
-    """Read back from the identity and version registers"""
+async def access_device(tb, log):
+    """Read back from the identity, version, and status registers"""
     ctrl = Control()
     # Read the identity register
     log.info("Reading back identity")
@@ -33,7 +33,7 @@ async def access_identity(tb, log):
         rd_data=int(ctrl.device.identity),
     ))
     # Read the version register
-    log.info("Reading back version")
+    log.info("Reading back version (build randomised 100 times)")
     for _ in range(100):
         version = Version()
         version.build = tb.random.getrandbits(ctrl.device.version.build._pt_width)
@@ -47,5 +47,21 @@ async def access_identity(tb, log):
         ))
         tb.scoreboard.channels["mon"].push_reference(RegResponse(
             rd_data=int(version),
+        ))
+        await tb.mon.wait_for(MonitorEvent.CAPTURE)
+    # Read back the status register
+    log.info("Reading back status (randomised 100 times)")
+    for _ in range(100):
+        sts = Status._pt_unpack(tb.random.getrandbits(Status._PT_WIDTH))
+        tb.internal.status.value = int(sts)
+        tb.internal.status_strobe.value = 1
+        await RisingEdge(tb.clk)
+        tb.internal.status_strobe.value = 0
+        tb.drv.enqueue(RegRequest(
+            address=ctrl.device.status._pt_offset,
+            write=False,
+        ))
+        tb.scoreboard.channels["mon"].push_reference(RegResponse(
+            rd_data=int(sts),
         ))
         await tb.mon.wait_for(MonitorEvent.CAPTURE)
