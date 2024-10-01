b248c5d578e44a7c2e0ea84a4da54f9455e41899
Peter Birch <peter.birch@vypercore.com>
2024-10-01 19:51:27 +0100

    Adding TODO to bridge

--- packtype/common/pt_axi4lite_bridge.sv
+++ packtype/common/pt_axi4lite_bridge.sv
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// TODO @intuity: Pretty sure the alignment conditions are wrong for AXI writes
+//                and won't work where AW and W arrive in different cycles
+
 module pt_axi4lite_bridge #(
       parameter  AXI_ADDR_W     = 32
     , parameter  RF_ADDR_W      = 32
