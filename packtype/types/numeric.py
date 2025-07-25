bd2bbb0357a7d835e111518aa2ed3f7be26f9d9b
Peter Birch <pbirch@fractile.ai>
2025-07-25 14:56:35 +0100

    Handling 'other' more subtly

--- packtype/types/numeric.py
+++ packtype/types/numeric.py
@@ -2,13 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-try:
-    from typing import Self
-except ImportError:
-    from typing_extensions import Self  # noqa: UP035
-
-from .base import MetaBase
-
 
 class Numeric:
     def __int__(self) -> int:
@@ -18,122 +11,195 @@ class Numeric:
     # Operator overrides
     # ==========================================================================
 
-    def __add__(self, other: int | Self) -> int:
-        return int(self) + int(other)
+    def __add__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) + other
 
-    def __sub__(self, other: int | Self) -> int:
-        return int(self) - int(other)
+    def __sub__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) - other
 
-    def __mul__(self, other: int | Self) -> int:
-        if isinstance(other, MetaBase):
-            return int(self) * other
-        else:
-            return int(self) * int(other)
+    def __mul__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) * other
 
-    def __truediv__(self, other: int | Self) -> int:
-        return int(self) / int(other)
+    def __truediv__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) / other
 
-    def __floordiv__(self, other: int | Self) -> int:
-        return int(self) // int(other)
+    def __floordiv__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) // other
 
-    def __mod__(self, other: int | Self) -> int:
-        return int(self) % int(other)
+    def __mod__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) % other
 
-    def __divmod__(self, other: int | Self) -> int:
-        return (int(self) // int(other), int(self) % int(other))
+    def __divmod__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return (int(self) // other, int(self) % other)
 
-    def __pow__(self, other: int | Self) -> int:
-        return int(self) ** int(other)
+    def __pow__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) ** other
 
-    def __lshift__(self, other: int | Self) -> int:
-        return int(self) << int(other)
+    def __lshift__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) << other
 
-    def __rshift__(self, other: int | Self) -> int:
-        return int(self) >> int(other)
+    def __rshift__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) >> other
 
-    def __and__(self, other: int | Self) -> int:
-        return int(self) & int(other)
+    def __and__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) & other
 
-    def __xor__(self, other: int | Self) -> int:
-        return int(self) ^ int(other)
+    def __xor__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) ^ other
 
-    def __or__(self, other: int | Self) -> int:
-        return int(self) | int(other)
+    def __or__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) | other
 
-    def __radd__(self, other: int | Self) -> int:
-        return int(other) + int(self)
+    def __radd__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other + int(self)
 
-    def __rsub__(self, other: int | Self) -> int:
-        return int(other) - int(self)
+    def __rsub__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other - int(self)
 
-    def __rmul__(self, other: int | Self) -> int:
-        return int(other) * int(self)
+    def __rmul__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other * int(self)
 
-    def __rtruediv__(self, other: int | Self) -> int:
-        return int(other) / int(self)
+    def __rtruediv__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other / int(self)
 
-    def __rfloordiv__(self, other: int | Self) -> int:
-        return int(other) // int(self)
+    def __rfloordiv__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other // int(self)
 
-    def __rmod__(self, other: int | Self) -> int:
-        return int(other) % int(self)
+    def __rmod__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other % int(self)
 
-    def __rdivmod__(self, other: int | Self) -> int:
-        return (int(other) // int(self), int(other) % int(self))
+    def __rdivmod__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return (other // int(self), other % int(self))
 
-    def __rpow__(self, other: int | Self) -> int:
-        return int(other) ** int(self)
+    def __rpow__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other ** int(self)
 
-    def __rlshift__(self, other: int | Self) -> int:
-        return int(other) << int(self)
+    def __rlshift__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other << int(self)
 
-    def __rrshift__(self, other: int | Self) -> int:
-        return int(other) >> int(self)
+    def __rrshift__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other >> int(self)
 
-    def __rand__(self, other: int | Self) -> int:
-        return int(other) & int(self)
+    def __rand__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other & int(self)
 
-    def __rxor__(self, other: int | Self) -> int:
-        return int(other) ^ int(self)
+    def __rxor__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other ^ int(self)
 
-    def __ror__(self, other: int | Self) -> int:
-        return int(other) | int(self)
+    def __ror__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return other | int(self)
 
-    def __iadd__(self, other: int | Self) -> int:
-        return int(self) + int(other)
+    def __iadd__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) + other
 
-    def __isub__(self, other: int | Self) -> int:
-        return int(self) - int(other)
+    def __isub__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) - other
 
-    def __imul__(self, other: int | Self) -> int:
-        return int(self) * int(other)
+    def __imul__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) * other
 
-    def __itruediv__(self, other: int | Self) -> int:
-        return int(self) / int(other)
+    def __itruediv__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) / other
 
-    def __ifloordiv__(self, other: int | Self) -> int:
-        return int(self) // int(other)
+    def __ifloordiv__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) // other
 
-    def __imod__(self, other: int | Self) -> int:
-        return int(self) % int(other)
+    def __imod__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) % other
 
-    def __ipow__(self, other: int | Self) -> int:
-        return int(self) ** int(other)
+    def __ipow__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) ** other
 
-    def __ilshift__(self, other: int | Self) -> int:
-        return int(self) << int(other)
+    def __ilshift__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) << other
 
-    def __irshift__(self, other: int | Self) -> int:
-        return int(self) >> int(other)
+    def __irshift__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) >> other
 
-    def __iand__(self, other: int | Self) -> int:
-        return int(self) & int(other)
+    def __iand__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) & other
 
-    def __ixor__(self, other: int | Self) -> int:
-        return int(self) ^ int(other)
+    def __ixor__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) ^ other
 
-    def __ior__(self, other: int | Self) -> int:
-        return int(self) | int(other)
+    def __ior__(self, other) -> int:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) | other
 
     def __neg__(self) -> int:
         return -1 * int(self)
@@ -150,23 +216,35 @@ class Numeric:
     def __invert__(self) -> int:
         return ~int(self)
 
-    def __lt__(self, other: int | Self) -> bool:
-        return int(self) < int(other)
+    def __lt__(self, other) -> bool:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) < other
 
-    def __le__(self, other: int | Self) -> bool:
-        return int(self) <= int(other)
+    def __le__(self, other) -> bool:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) <= other
 
-    def __eq__(self, other: int | Self) -> bool:
-        return int(self) == int(other)
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) == other
 
-    def __ne__(self, other: int | Self) -> bool:
-        return int(self) != int(other)
+    def __ne__(self, other) -> bool:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) != other
 
-    def __gt__(self, other: int | Self) -> bool:
-        return int(self) > int(other)
+    def __gt__(self, other) -> bool:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) > other
 
-    def __ge__(self, other: int | Self) -> bool:
-        return int(self) >= int(other)
+    def __ge__(self, other) -> bool:
+        if isinstance(other, Numeric):
+            other = int(other)
+        return int(self) >= other
 
     def __hash__(self) -> int:
         return id(self)
